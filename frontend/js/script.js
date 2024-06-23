86651c9be715041cd4cc936137490fd9ee4c33b3
SavioGuratsu <savioguratsu@gmail.com>
2024-06-23 18:09:42 -0300

    changes server url

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -89,7 +89,7 @@ const handleLogin = (event) => {
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket("ws://localhost:8080")
+    websocket = new WebSocket("wss://https://chat-xmls.onrender.com")
     websocket.onmessage = processMessage
 }
 
